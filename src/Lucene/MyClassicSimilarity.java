51c5524cbb7166121a3e0984178d46522c66f0ca
Dor <cohensdor@gmail.com>
2018-06-08 21:57:47 +0300

    Add myclassicsimilarity

---
+++ src/Lucene/MyClassicSimilarity.java
@@ -0,0 +1,29 @@
+package Lucene;
+
+import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+
+public class MyClassicSimilarity extends ClassicSimilarity {
+
+    @Override
+    public float tf(float freq){
+        float t = super.tf(freq);
+        return t*t;
+    }
+
+    @Override
+    public float idf(long docFreq , long docCount){
+        return (float)Math.log10(docCount/docFreq);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
